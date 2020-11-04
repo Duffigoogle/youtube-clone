7f48541c2d1c57a9d00f0335624e10f101b5dc5d
Caleb Duff <calebdduff@gmail.com>
2020-11-04 14:30:53 +0100

    initial commit, Firebase installed.

---
+++ youtube-clone/src/components/Firebase/firebase.js
@@ -0,0 +1,20 @@
+// FirebaseConfig.js
+// Contains the config details of firebase, this should not be committed to your repo.
+// If the repo is public
+// You could optionally use .env file to store data.
+
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBYNgtCktw0Tw2KfKxl8NgmOGoVTKv7v4k",
+    authDomain: "clone-87347.firebaseapp.com",
+    databaseURL: "https://clone-87347.firebaseio.com",
+    projectId: "clone-87347",
+    storageBucket: "clone-87347.appspot.com",
+    messagingSenderId: "1087776850247",
+    appId: "1:1087776850247:web:b8ffd04b1510aed9e94c62"
+  };
+
+firebase.initializeApp(firebaseconfig);
+
+export default firebase;
