e250f44551c77991565bb9a4dacc15fd64427a49
Caleb Duff <calebdduff@gmail.com>
2020-11-11 21:32:14 +0100

    updated component features

--- youtube-clone/src/components/TopBar.js
+++ youtube-clone/src/components/TopBar.js
@@ -1,61 +1,104 @@
 import React from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
-import { IconButton, makeStyles} from '@material-ui/core/styles';
+import { IconButton, makeStyles, fade, AppBar, Toolbar, useTheme } from '@material-ui/core';
+import  MoreVert from '@material-ui/icons/MoreVert';
+import  VideoCall from '@material-ui/icons/VideoCall';
+import  Apps  from '@material-ui/icons/Apps';
+import { Search as SearchIcon } from '@material-ui/icons';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import SearchIcon from '@material-ui/icons/Search';
-import MenuIcon from '@material-ui/icons/Menu';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import { InputBase } from '@material-ui/core';
+// import InputBase from '@material-ui/core/InputBase';
+import TopBarSearch from './TopBarSearch';
 
 
 const useStyles = makeStyles((theme) => ({
-    button: {
+    menuButton: {
         margin: theme.spacing(1),
+    },
+    logo : {
+        height: 30,
+        cursor: 'pointer'
     },
     icons: {
         paddingRight: 10,
         color: '#606060'
     },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        // position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+    },
+    appBar: {
+        position: 'fixed',
+        backgroundColor: 'grey',
+        color: 'inherit',
+        boxShadow: 'none',
+        font: '14px Roboto, Arial, sans-serif'
+
+    }
 }));
 
 function TopBar() {
     const classes = useStyles();
+    const theme = useTheme();
+
     return (
         <div>
-            <IconButton edge='start'
+            <AppBar position='static'
+                    className={classes.AppBar}
+                    style={{ backgroundColor: theme.palette.background.drawer, color: theme.palette.background.text}}>
+                <Toolbar>
+                    <IconButton edge='start'
+                                className={classes.menuButton}
+                                color='inherit'
+                                aria-label='open drawer'>
+                        <MenuIcon />
+                    </IconButton>
+                    <img src={theme.palette.background.logo} className={classes.logo} alt='logo' />
+                    <div className={classes.search}>
+                        {/* <InputBase 
+                            placeholder='Search' 
+                            classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{'aria-label': 'search'}}
+                        /> */}
+                        <TopBarSearch />
+                    </div>
+                    <IconButton className={classes.icons} style={{color: theme.palette.background.icons}}>
+                        <VideoCall />
+                    </IconButton>
+                    <IconButton className={classes.icons} style={{color: theme.palette.background.icons}}>
+                        <Apps />
+                    </IconButton>
+                    <IconButton className={classes.icons} style={{color: theme.palette.background.icons}}>
+                        <MoreVert />
+                    </IconButton>
+                    <Button 
+                        // onClick={handleLogin}
+                        color='secondary' 
+                        variant='outlined' 
                         className={classes.menuButton}
-                        color='inherit'
-                        aria-label='open drawer'>
-                <MenuIcon />
-            </IconButton>
-            <img src={theme.palette.background.logo} className={classes.logo} alt='logo' />
-            <div className={classes.search}>
-                <InputBase 
-                    placeholder='Search' 
-                    classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                }}
-                inputProps={{'aria-label': 'search'}}
-                />
-                <SearchIcon />
-            </div>
-            <IconButton className={classes.icons} style={{color: }}>
-                <VideoCall />
-            </IconButton>
-            <IconButton className={classes.icons} style={{color: }}>
-                <Apps />
-            </IconButton>
-            <IconButton >
-                <MoreVert />
-            </IconButton>
-            <Button 
-                color='secondary' 
-                variant='outlined' 
-                className={classes.button}
-                startIcon={<AccountCircle/>}> SIGN IN
-            </Button>
+                        startIcon={<AccountCircle/>}> SIGN IN
+                    </Button>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
