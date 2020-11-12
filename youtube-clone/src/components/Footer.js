39d9e3ceac3ec6c283b8ad960c7686cd4503be75
Caleb Duff <calebdduff@gmail.com>
2020-11-12 15:16:22 +0100

    Added Footer Component

---
+++ youtube-clone/src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <div>
+                <ul>
+                    <li>About</li>
+                    <li>Press</li>
+                    <li>Copyright</li>
+                    <li>Contact us</li>
+                    <li>Creators</li>
+                    <li>Advertise</li>
+                    <li>Developers</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Terms</li>
+                    <li>Privacy</li>
+                    <li>Policy & Safety</li>
+                    <li>How YouTube works</li>
+                    <li>Test new features</li>
+                </ul>
+            </div>
+            <div>
+                <h3>&copy; 2020 Google LLC</h3>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Footer
